6fcea60c59c5417ad49ccbf5f0d87ea9fab97069
Quinntyne Brown <quinntynebrown@gmail.com>
2015-12-15 07:01:34 -0500

    update arrow function example

--- wwwroot/arrowFunctions/app.ts
+++ wwwroot/arrowFunctions/app.ts
@@ -1,4 +1,7 @@
-﻿class NameService {
+﻿//https://channel9.msdn.com/Blogs/One-Dev-Minute/Arrow-Functions-in-JavaScript
+//https://aka.ms/arrowfunctions
+
+class NameService {
     constructor(private name: any) { }
 
     getFullname = (prefix:string) => prefix + this.name;
@@ -6,7 +9,8 @@
 
 var nameService = new NameService("Quinntyne Brown");
 
-// fat arrow will return the right side automatically
+// return is implicit when using the arrow function and single line right side
+// return is explicit if right side is more than one line
 // should alert 'Mr. Quinntyne Brown'
 // you can remove brackets ( ) if you leave out the type ':string'
 alert(nameService.getFullname("Mr. "));
